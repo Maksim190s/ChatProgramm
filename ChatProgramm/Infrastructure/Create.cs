3300f9045096563273c805949dd5103ac540a663
menswear <MAZAKSIM@outlook.com>
2022-07-15 18:14:29 +0800

    new classes: Create, Write

---
+++ ChatProgramm/Infrastructure/Create.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace ChatProgramm.Infrastructure
+{
+    public class Create
+    {
+        /*
+        public static Message NewMessage()
+        {
+            
+            Console.WriteLine("Author:");
+            var nickname = Console.ReadLine();
+            Console.WriteLine("Enter a message:");
+            var text = Console.ReadLine();
+            var user = FindUserByNickname();
+            return;
+        }
+
+        private  User FindUserByNickname(User[] users, string n)
+        {
+            
+            foreach (var u in users)
+            {
+                if (u.Nickname.ToUpper() == n.ToUpper())
+                    return u;
+            }
+            return null;
+        }
+
+
+        public static Like NewLike()
+        {
+            return;
+        }
+        */
+    }
+
+}
+
---
+++ ChatProgramm/Infrastructure/Write.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace ChatProgramm.Infrastructure
+{
+	public class Write
+	{
+		public static void Message(string path, string Nick, string Text, string Likes)
+        {
+			File.AppendAllText(path, $"</|> MESSAGE </|>\n{Nick}<+>{Text}<+>{Likes}\n");
+        }
+
+		
+
+		
+	}
+}
+
