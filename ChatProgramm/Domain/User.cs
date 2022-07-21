9e4cbfc7207b077f51c911878cf2822703db28ee
menswear <MAZAKSIM@outlook.com>
2022-07-21 16:23:05 +0800

    added field ID in User record; added ID in DB-Users

--- ChatProgramm/Domain/User.cs
+++ ChatProgramm/Domain/User.cs
@@ -3,16 +3,9 @@ namespace ChatProgramm
 {
 	public record User(string FullName, DateTime DateOfBirth, string Nickname)
     {
-        //private DateTime DateOfBirth { get; init; } = DateOfBirth;
-        /*
-        public bool IsLeagalAge()
-        {
-            var now = DateTime.Now;
-            var age = now - DateOfBirth;
-            return age.Days > (365 * 18);
-        }
-        */
+        public int Id { get; set; }
 
+       
       
        
     }	
