eb4e30daacc3eacd59752f30fce572bfc3dc253c
menswear <MAZAKSIM@outlook.com>
2022-07-21 16:28:44 +0800

    fixed objects(user/chatRepo); variables in domain structure

--- ChatProgramm/Program.cs
+++ ChatProgramm/Program.cs
@@ -6,24 +6,24 @@ using System.Text.Json;
 
 // infrastructure
 
-var userRepository = new UserRepository();
-var chatRepository = new ChatRepository();
+var userRepository = new UserRepository("DataBase/Users.json");
+var chatRepository = new ChatRepository("DataBase/Chat.json");
 var create = new Create();
 
 
 // domain
 
-var allUsers = userRepository.LoadUsers("DataBase/Users.json");
-var chat = chatRepository.LoadChat("DataBase/Chat.json", allUsers);
+var allUsers = userRepository.LoadUsers();
+var chat = chatRepository.LoadChat(allUsers);
 var allMessages = chat.Messages;
 
 var newMessage = create.NewMessageByUser(allUsers);
-var newLike = create.NewLikeForMessage(allMessages, allUsers);
+//var newLike = create.NewLikeForMessage(allMessages, allUsers);
 
 
 // infrastructure
 chat.AddNewMessage(newMessage);
-chat.AddNewLike(newLike);
+//chat.AddNewLike(newLike);
 
 Write.Chat("DataBase/Chat.json", chat);
-Write.Users("DataBase/Users.json", allUsers);
+//Write.Users("DataBase/Users.json", allUsers);
