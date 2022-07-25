575e5a9dd42e908df41abcfba4f1c375fd772639
menswear <MAZAKSIM@outlook.com>
2022-07-25 11:41:33 +0800

    added UserDto record

--- ChatProgramm/Infrastructure/UserRepository.cs
+++ ChatProgramm/Infrastructure/UserRepository.cs
@@ -6,12 +6,27 @@ namespace ChatProgramm
 {
     public class UserRepository
     {
-        public User[] LoadUsers(string path)
+        private string _filePath;
+
+        public UserRepository(string filePath)
         {
-            var userText = File.ReadAllText(path);
-            var users = JsonSerializer.Deserialize<List<User>>(userText, new JsonSerializerOptions { PropertyNameCaseInsensitive = true, WriteIndented = true });
-            return users.ToArray();
+            this._filePath = filePath;
+        }
+
+        public User[] LoadUsers()
+        {
+            // domain
+
+            var userText = File.ReadAllText(_filePath);
+            var users = JsonSerializer.Deserialize<List<UserDto>>(userText, new JsonSerializerOptions { PropertyNameCaseInsensitive = true, WriteIndented = true });
+            var allUsers = new List<User>();
+            foreach (var user in users)
+            {
+                allUsers.Add(new User(user.FullName, user.DateOfBirth, user.Nickname));
+            }
+            return allUsers.ToArray();
         }
     }
+        record UserDto (string FullName, DateTime DateOfBirth, string Nickname);
 }
 
